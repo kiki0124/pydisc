85dc5e9c53bc57395ce2d8e032210021c223a6e9
kiki124 <kiki1241245@gmail.com>
2024-10-26 22:25:24 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="PyDisc",
+    package=find_packages(include=["PyDisc"]),
+    version='0.1.0',
+    description="A Python wrapper for connecting with the Discord API (under development)",
+)
